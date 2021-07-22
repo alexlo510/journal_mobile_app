b1c4a46bf7c25c9ff63239a475cb17bf9ee0a35b
Alex Lo <alexlo493@gmail.com>
2021-07-22 11:18:30 -0700

    added routes and the screens to those routes

--- journal/lib/app.dart
+++ journal/lib/app.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:journal/screens/journal_entry.dart';
+import 'package:journal/screens/journal_entry_list.dart';
+import 'package:journal/screens/new_journal_entry.dart';
 
 class App extends StatelessWidget {
+
+  static final routes = {
+    '/': (context) => JournalEntryList(),
+    'journalEntry': (context) => JournalEntry(),
+    'newJournalEntry': (context) => NewJournalEntry(),
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -9,27 +18,24 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Journal')),
-        body: LayoutBuilder(builder: layoutDecider),
-      ),
+      routes: routes,
     );
   }
-
-  Widget layoutDecider(BuildContext context, BoxConstraints constraints) =>
-    constraints.maxWidth < 800 ? VerticalLayout() : HorizontalLayout();
 }
 
-class VerticalLayout extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text('Vertical');
-  }
-}
+// Widget layoutDecider(BuildContext context, BoxConstraints constraints) =>
+//     constraints.maxWidth < 800 ? VerticalLayout() : HorizontalLayout();
 
-class HorizontalLayout extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text('Horizontal');
-  }
-}
+// class VerticalLayout extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text('Vertical');
+//   }
+// }
+
+// class HorizontalLayout extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text('Horizontal');
+//   }
+// }
---
+++ journal/lib/screens/new_journal_entry.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewJournalEntry extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('New Journal Entry')),
+      body: Text('New Journal Entry'),
+    );
+  }
+}
