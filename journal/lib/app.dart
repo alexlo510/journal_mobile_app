211d9b38170431b8347be663d125b1b6e4866110
Alex Lo <alexlo493@gmail.com>
2021-07-22 10:40:23 -0700

    added layout decider

--- journal/lib/app.dart
+++ journal/lib/app.dart
@@ -11,8 +11,25 @@ class App extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text('')),
-        body: LayoutBuilder(builder: ), 
+        body: LayoutBuilder(builder: layoutDecider),
+      ),
     );
   }
-  
+
+  Widget layoutDecider(BuildContext context, BoxConstraints constraints) =>
+    constraints.maxWidth < 800 ? VerticalLayout() : HorizontalLayout();
+}
+
+class VerticalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Vertical');
+  }
+}
+
+class HorizontalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Horizontal');
+  }
 }
