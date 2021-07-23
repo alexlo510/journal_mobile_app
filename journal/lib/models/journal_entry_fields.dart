9ebe20d042dd973b71f7ade4ec5490c6ba7e94f5
Alex Lo <alexlo493@gmail.com>
2021-07-23 15:53:18 -0700

    added class for journal entry fields

---
+++ journal/lib/models/journal_entry_fields.dart
@@ -0,0 +1,17 @@
+class JournalEntryFields {
+  String? title;
+  String? body;
+  int? rating;
+  DateTime? dateTime;
+
+  JournalEntryFields({
+    this.title,
+    this.body,
+    this.rating,
+    this.dateTime
+  });
+
+  String toString() {
+    return 'Title: $title, Body: $body, Rating: $rating, Time: $dateTime';
+  }
+}
