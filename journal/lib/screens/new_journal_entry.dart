5a48c4a217b15f6049cafde7723e1a71fda06d6c
Alex Lo <alexlo493@gmail.com>
2021-07-22 17:44:41 -0700

    added imports and endDrawer

--- journal/lib/screens/new_journal_entry.dart
+++ journal/lib/screens/new_journal_entry.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:journal/components/journal_drawer.dart';
+import 'package:journal/components/journal_scaffold.dart';
+import 'package:journal/screens/journal_entry_list.dart';
 
 class NewJournalEntry extends StatelessWidget {
 
   static const routeName = 'newJournalEntry';
-
+  
   @override
   Widget build(BuildContext context) {
+
+    final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
+    
     return Scaffold(
-      appBar: AppBar(title: Text('New Journal Entry')),
+      appBar: AppBar(
+        //leading: Icon(Icons.ac_unit),
+        title: Text('New Journal Entry'),
+        actions: [openSettings()],
+      ),
+      endDrawer: LayoutBuilder(builder: (context, constraints) => drawerDecider(context, constraints, args.darkMode, args.toggleTheme)),
       body: Text('New Journal Entry'),
     );
   }
 }
+
