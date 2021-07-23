eac4110f1ad4d9afe7e59701471b359fd5e0d563
Alex Lo <alexlo493@gmail.com>
2021-07-22 19:14:29 -0700

    remove constructors

--- journal/lib/screens/journal_entry_list.dart
+++ journal/lib/screens/journal_entry_list.dart
@@ -7,19 +7,10 @@ class JournalEntryList extends StatelessWidget {
 
   static const routeName = '/';
 
-  final darkMode;
-  final toggleTheme;
-
-  JournalEntryList({
-    Key? key,
-    this.darkMode,
-    this.toggleTheme,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      endDrawer: LayoutBuilder(builder: (context, constraints) => drawerDecider(context, constraints, darkMode, toggleTheme)),
+      endDrawer: LayoutBuilder(builder: drawerDecider),
       appBar: AppBar(
         title: Text('Journal Entry List'),
         actions: [openSettings()],
@@ -27,16 +18,13 @@ class JournalEntryList extends StatelessWidget {
       body: LayoutBuilder(builder: layoutDecider),
       floatingActionButton: FloatingActionButton(
         child : Icon(Icons.add),
-        onPressed: () {pushNewJournalEntry(context, ScreenArguments(darkMode, toggleTheme));},
+        onPressed: () {pushNewJournalEntry(context);},
       ),
     );
   }
 
   Widget layoutDecider(BuildContext context, BoxConstraints constraints) =>
     constraints.maxWidth < 800 ? VerticalLayout(): HorizontalLayout();
-
-  // Widget drawerDecider(BuildContext context, BoxConstraints constraints) =>
-  //   constraints.maxWidth < 800 ? verticalDrawer(context, drawer(darkMode, toggleTheme)) : drawer(darkMode, toggleTheme);
 
 }
 
@@ -54,13 +42,6 @@ class HorizontalLayout extends StatelessWidget {
   }
 }
 
-void pushNewJournalEntry(BuildContext context, ScreenArguments args) {
-  Navigator.of(context).pushNamed(NewJournalEntry.routeName, arguments: args );
-}
-
-class ScreenArguments {
-  final bool darkMode;
-  final Function toggleTheme;
-
-  ScreenArguments(this.darkMode, this.toggleTheme);
+void pushNewJournalEntry(BuildContext context) {
+  Navigator.of(context).pushNamed(NewJournalEntry.routeName);
 }
