1fd547a86be3e9c1f0309065836729dfb52ea7ac
Alex Lo <alexlo493@gmail.com>
2021-07-23 17:49:05 -0700

    added constructor for testing

--- journal/lib/models/journal.dart
+++ journal/lib/models/journal.dart
@@ -1,10 +1,20 @@
-import 'package:journal/screens/journal_entry.dart';
+import 'Package:journal/models/journal_entry.dart';
 
 class Journal {
   List<JournalEntry> journalEntriesList;
 
   Journal({
-    required this.journalEntriesList,
-  });
+    journalEntriesList,
+  }) : journalEntriesList = journalEntriesList ?? [];
+
+  Journal.fake() : journalEntriesList = [
+    JournalEntry(
+        title: 'Test Title',
+        body: 'Test Body',
+        rating: 1,
+    )
+  ];
+
+  get isEmpty => true; // use as test for now
 
 }
