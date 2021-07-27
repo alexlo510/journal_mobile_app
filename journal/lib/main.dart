058f9b383e5ce1ac9ae30cc382b7a3fff09d49e8
Alex Lo <alexlo493@gmail.com>
2021-07-27 11:35:03 -0700

    added initilize of singleton database class

--- journal/lib/main.dart
+++ journal/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:journal/app.dart';
+import 'package:journal/db/database_manager.dart';
+import 'package:sqflite/sqflite.dart'; // delete later if not deleting db
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +12,8 @@ void main() async{
     DeviceOrientation.landscapeRight,
     DeviceOrientation.portraitUp,
   ]);
+  await deleteDatabase('journal.sqlite3.db'); // remove later
+  await DatabaseManager.initialize();
   runApp(App(
     preferences: await SharedPreferences.getInstance())
   );
